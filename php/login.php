af3ba997c2f5608614da24eb0af93e003b2b7202
Victor Manuel de Barros Garcia <victor.garcia@prill.com.br>
2021-08-30 21:39:19 -0300

    Resolvido problema no login.php/ login funcional

--- php/login.php
+++ php/login.php
@@ -7,23 +7,32 @@ if(empty($_POST['email']) || empty($_POST['senha'])){
     exit();
 }
 
-$Lemail = mysqli_real_escape_string($conn, $_POST['email']);
-$Lsenha = mysqli_real_escape_string($conn, $_POST['senha']);
+$Lemail = $_POST['email'];
+$Lsenha  = $_POST['senha'];
 
-$query = "SELECT email FROM cliente WHERE email = '{$Lemail}' AND senha = md5('{$Lsenha}')";
+$sql = "SELECT senha FROM cliente WHERE email = '{$Lemail}'";
+$result = $conn->query($sql);
 
-$result = mysqli_query($conn,$query);
+if ($result->num_rows > 0) {
 
-$numrow = mysqli_num_rows($result);
-
-if($numrow == 1){      
-    $_SESSION['email'] = $Lemail;
-    header('Location:../index.php');
-    exit();
-}else{
-    $_SESSION['nao_autenticado'] = true;
-    header('Location:../EntrarSistema.php');
-    exit();
+    //echo "Senha encontrada no BD" . "<br>";
+  
+    while($row = $result->fetch_assoc()) {
+        //echo "Senha: " . $row["senha"] . "<br>";
+        $hash = $row["senha"];
+    }
+    
+    if (password_verify($Lsenha, $hash)) {
+        //echo 'Senha Valida';
+        $_SESSION['email'] = $Lemail;
+        header('Location:../index.php');
+    } else {
+        //echo 'Senha Invalida';
+        //$_SESSION['mensagem'] = "<div id='btms' style='background-color:red;widht:100%;color:white;text-align:center;padding:1%'>Usuário ou Senha Incorretos</div>";
+        $_SESSION['nao_autenticado'] = true;
+        header('Location:../EntrarSistema.php');
+    }
+} else {
+    echo "<br>" . "Erro senha não encontrada: " . "<br>" . $sql . "<br>" . $conn->error;
 }
-
 ?>
