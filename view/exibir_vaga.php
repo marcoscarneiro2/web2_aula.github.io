10559815e151ea4c30248807f4495fc2215d135c
Marcos Paulo Carneiro Tavares <46692865+marcoscarneiro2@users.noreply.github.com>
2021-11-18 23:21:47 -0300

    Criando tela de exibição de vaga

---
+++ view/exibir_vaga.php
@@ -0,0 +1,53 @@
+<?php 
+include_once '../assets/components/navbar.php';
+?>
+    
+    <div class="mainvaga">
+        <div class="cardcontainervaga">
+            <div class="titulocard">
+                <div id="cd1">
+                    <h1>Programador - Php</h1>
+                    <h3>R$ 2.500,00</h3>
+                    <p>Rio de Janeiro - RJ</p>
+                </div>
+                <div id="cd2">
+                <input id="botao" type="button" value="Candidatar-se"><br><br>
+                    <span>12 candidaturas para a vaga</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="cardcontainervaga">
+            <div class="descricao">
+                <h2><b>Quem somos:</b></h2>
+                    <p>Somos a Americanas sa, que consiste na poderosa combinação de múltiplas plataformas de negócios: B2W Digital, Lojas Americanas, Ame Digital e LET’S, concebidas com o ambicioso propósito de oferecer aos nossos clientes “Tudo. A toda hora. Em qualquer lugar”.</p>
+                <h2><b>Temos um desafio para pessoas que:</b></h2>
+                    <p>Somos a Americanas sa, que consiste na poderosa combinação de múltiplas plataformas de negócios: B2W Digital, Lojas Americanas, Ame Digital e LET’S, concebidas com o ambicioso propósito de oferecer aos nossos clientes “Tudo. A toda hora. Em qualquer lugar”.</p>
+                <h2><b>Atividades que farão parte do seu dia a dia aqui na Americanas:</b></h2>
+                    <p>Somos a Americanas sa, que consiste na poderosa combinação de múltiplas plataformas de negócios: B2W Digital, Lojas Americanas, Ame Digital e LET’S, concebidas com o ambicioso propósito de oferecer aos nossos clientes “Tudo. A toda hora. Em qualquer lugar”.</p>  
+                <h2><b>O que buscamos nessa pessoa que é importante para nós:</b></h2>
+                    <p>Somos a Americanas sa, que consiste na poderosa combinação de múltiplas plataformas de negócios: B2W Digital, Lojas Americanas, Ame Digital e LET’S, concebidas com o ambicioso propósito de oferecer aos nossos clientes “Tudo. A toda hora. Em qualquer lugar”.</p>
+                <h2><b>O que oferecemos para nossos associados:</b></h2>
+                    <p>Somos a Americanas sa, que consiste na poderosa combinação de múltiplas plataformas de negócios: B2W Digital, Lojas Americanas, Ame Digital e LET’S, concebidas com o ambicioso propósito de oferecer aos nossos clientes “Tudo. A toda hora. Em qualquer lugar”.</p>
+       
+            </div>
+        </div>
+
+        <div class="cardcontainervaga">
+            <div class="descricao">
+                <h2><b>Sobre a empresa</b></h2>
+                 <p>Somos a Americanas sa, que consiste na poderosa combinação de múltiplas plataformas de negócios: B2W Digital, Lojas Americanas, Ame Digital e LET’S, concebidas com o ambicioso propósito de oferecer aos nossos clientes “Tudo. A toda hora. Em qualquer lugar”.</p>
+        
+            </div>
+        </div>
+
+            <div class="cardcontainervaga">
+                <div id="btt">
+                <input id="botao" type="button" value="Candidatar-se">
+                </div>
+            </div>
+        </div>
+    
+<?php 
+include_once '../assets/components/footer.php';
+?> 
